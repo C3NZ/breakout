c6132cc1bba515d6caee393a73a0324bba1a3a0e
C3NZ <cenz@cenz.io>
2018-12-16 13:04:44 -0800

    [feat] add eslint and start cleaning up code base

--- public/js/game.js
+++ public/js/game.js
@@ -1,54 +1,39 @@
-import {Level} from './levels.js';
-import {Player} from './player.js';
-import {Ball} from './ball.js'
-import {InputHandler} from './utils.js';
+import { Level } from './levels.js';
+import { Player } from './player.js';
+import { Ball } from './ball.js'
+import { InputHandler } from './utils.js';
 
-//wrapper function to start the game with customizable options.
-//You can customize the canvas, level, and player that you can use 
-export function startGame(options={}) {
-    const canvas = options.canvas || document.getElementById("myCanvas") 
-    const ctx = canvas.getContext("2d");
-    let level = options.level || new Level(canvas);
-    let player = options.player || new Player(canvas);
-    let ball = options.ball || new Ball(canvas);
-    let entities = {
-        level: level, 
-        ball: ball, 
-        player: player
-    }
-    const game = new Game(canvas, ctx, entities);
-    function play() {
-        game.play();
-        requestAnimationFrame(play);
-    }
-    play();
-}
-
-//Instance of the game.
-//Concerned with in game updating and rendering
+// Instance of the game.
+// Concerned with in game updating and rendering
 class Game {
     constructor(canvas, ctx, entities) {
         this.canvas = canvas;
         this.ctx = ctx;
         this.entities = entities;
-        
+
+        // Iterate through all entities
+        // eslint-disable-next-line
         for (const entity of Object.keys(entities)) {
             this.entities[entity].attachGame(this);
         }
-        
+
         this.inputHandler = new InputHandler(this);
         this.inputHandler.initHandlers();
         this.running = true;
     }
 
     update() {
+        // Iterate through all entities
+        // eslint-disable-next-line
         for(const entity of Object.keys(this.entities)) {
             this.entities[entity].update();
         }
     }
-    
+
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        // Iterate through all entities
+        // eslint-disable-next-line
         for(const entity of Object.keys(this.entities)) {
             this.entities[entity].draw(this.ctx);
         }
@@ -58,7 +43,30 @@ class Game {
         this.update();
         this.draw();
     }
-
 }
+
+// wrapper function to start the game with customizable options.
+// You can customize the canvas, level, and player that you can use 
+export function startGame(options = {}) {
+    const canvas = options.canvas || document.getElementById('myCanvas');
+    const ctx = canvas.getContext('2d');
+    const level = options.level || new Level(canvas);
+    const player = options.player || new Player(canvas);
+    const ball = options.ball || new Ball(canvas);
+    const entities = {
+        level,
+        ball,
+        player,
+    };
+    const game = new Game(canvas, ctx, entities);
+
+    // Closure to keep running the game over and over again
+    function play() {
+        game.play();
+        requestAnimationFrame(play);
+    }
+    play();
+}
+
 
 startGame();
