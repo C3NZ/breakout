d31cb204b57608f8b88f77b3da8617d8405db81d
C3NZ <cenz@cenz.io>
2018-12-23 01:14:51 -0800

    [feat] fix app to be ran on heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use('/', express.static('public'));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     // eslint-disable-next-line
     console.log('listening on port 3000')
 });
